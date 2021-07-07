e785e83db7d138a8b8413533bde8211e7e012883
kevin-j-ferrell <kferrell@apple.com>
2021-07-07 15:27:47 -0400

    Add steps to export developer signed build

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 echo "Running custom post build script"
+
+xcodebuild -exportArchive -archivePath $CI_ARCHIVE_PATH -exportOptionsPlist "${CI_WORKSPACE}/ExportOptions.plist" -allowProvisioningUpdates -exportPath $CI_WORKSPACE
+
+echo "Attempted signed export"
