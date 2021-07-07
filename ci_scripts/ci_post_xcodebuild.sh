ab2fdd8324600fabd4b8742a740277b9ebbde8e1
kevin-j-ferrell <kferrell@apple.com>
2021-07-07 15:40:14 -0400

    Update post build script

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -2,6 +2,12 @@
 
 echo "Running custom post build script"
 
-xcodebuild -exportArchive -archivePath $CI_ARCHIVE_PATH -exportOptionsPlist "${CI_WORKSPACE}/ExportOptions.plist" -allowProvisioningUpdates -exportPath $CI_WORKSPACE
+echo "Writing ASC key to file"
+
+echo "$ASC_API_KEY" >> "${CI_WORKSPACE}/ASC_KEY.p8"
+
+#xcodebuild -exportArchive -archivePath $CI_ARCHIVE_PATH -exportOptionsPlist "${CI_WORKSPACE}/ExportOptions.plist" -allowProvisioningUpdates -exportPath $CI_WORKSPACE
+
+xcodebuild -exportArchive -archivePath $CI_ARCHIVE_PATH -exportOptionsPlist "${CI_WORKSPACE}/ExportOptions.plist" -authenticationKeyIssuerID 69a6de71-cbb0-47e3-e053-5b8c7c11a4d1 -authenticationKeyID M37FNNDGV3 -authenticationKeyPath "${CI_WORKSPACE}/ASC_KEY.p8" -allowProvisioningUpdates -exportPath $CI_WORKSPACE
 
 echo "Attempted signed export"
