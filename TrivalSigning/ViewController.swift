07f0f0eb23f0d379e575b6876562992617ce7f70
kevin-j-ferrell <kferrell@apple.com>
2021-07-07 12:55:04 -0400

    Initial Commit

---
+++ TrivalSigning/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TrivalSigning
+//
+//  Created by Kevin Ferrell on 7/7/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
